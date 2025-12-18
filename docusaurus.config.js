fb1ecc7d8627f7c50a069c3ba4193f26d64f9b36
Laiba Parvez <164337174+whoizlaiba@users.noreply.github.com>
2025-12-19 03:59:51 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://digital-wellness-book.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'whoizlaiba', // Usually your GitHub org/user name.
+  projectName: 'Digital-wellness-book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
